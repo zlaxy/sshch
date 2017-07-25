4ec5c9677581537542f461413b0bb7eee2a79fb8
zlaxy <zlaxyi@gmail.com>
2017-07-25 13:07:47 +0300

    add setup file and update readme

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-Simpe python script for fast access to ssh hosts.
-
-under DWTWL 2.5 license: https://soundragon.su/license/license.html
+SSH connection manager with curses interface
+======
+sshch is released under DWTWL 2.5 license
+![sshch](https://raw.githubusercontent.com/zlaxy/sshch/master/sshch_screenshot.png)
+### Installing
+To install for all users:
+```
+sudo python setup.py install
+```
+To install just for current user:
+```
+mkdir ~/.local/bin
+cp sshch/sshch ~/.local/bin/
+```
+### Using
+To run curses interface:
+```
+sshch
+```
+To run command line help:
+```
+sshch -h
+```
+**If you want to use unsafe 'password' feature you must install 'sshpass' first.**
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""Setup script for sshch"""
+
+def main():
+    from distutils.core import setup
+
+    setup(name='sshch',
+          author='zlaxy',
+          url='https://github.com/zlaxy/sshch/',
+          description='Ssh connection manager',
+          license='DWTWL 2.5',
+          version='0.55',
+          py_modules=['sshch'],
+          scripts=['sshch/sshch'],
+
+          # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+          classifiers=[
+              'Development Status :: 4 - Beta',
+              'Environment :: Console :: Curses',
+              'Intended Audience :: System Administrators',
+              'License :: Freeware',
+              'Natural Language :: English',
+              'Operating System :: POSIX',
+              'Programming Language :: Python :: 2.7',
+              'Topic :: Internet',
+              'Topic :: System :: Networking',
+              'Topic :: System :: Systems Administration',
+              'Topic :: Utilities'])
+
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
